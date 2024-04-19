84ca1434ef6b229d5bc95ce9dd27b5266cb09edd
Nikhil1O1 <technikue2.0@gmail.com>
2024-04-19 09:50:45 +0530

    ui changes and added self filter in online people (not wokring filter rn)

---
+++ client/src/Avatar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Avatar(userId) {
+  return (
+    <div className='w-6 h-6 bg-green-500 rounded-full'></div>
+  )
+}
+
+export default Avatar
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Avatar from './Avatar';
+import Logo from './Logo.jsx';
+import { UserContext } from './UserContext';
 
 export default function Chat() {
     const [ws, setWs] = useState(null)
     const [onlinePeople, setOnlinePeople] = useState({})
+    const [selectedUserId, setSelectedUserId] = useState(null)
+    const {username, id} = useContext(UserContext);
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000')
         setWs(ws);
@@ -24,17 +29,25 @@ export default function Chat() {
             showOnlinePeople(messageData.online)
         }
     }
+
+    const onlinePeopleFilter = {...onlinePeople};
+    delete onlinePeopleFilter[id];
   return (
     <div className='flex h-screen'>
-        <div className='bg-gradient-to-r from-gray-700 to-cyan-300 text-white w-1/3 p-2'>
+        <div className='bg-gradient-to-r from-gray-700 to-cyan-300 text-white w-1/3'>
+            <Logo/>
+            {username}
             {Object.keys(onlinePeople).map(userId => (
-                <div>
-                    {onlinePeople[userId]}
+                <div key = {userId} onClick={() => setSelectedUserId(userId)} className={'border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer '+(userId === selectedUserId ? 'bg-gray-600' : '')}>
+                    <Avatar/>
+                    <span>{onlinePeople[userId]}</span>
                 </div>
             ))}
         </div>
         <div className='flex flex-col bg-black text-white w-2/3 p-2'>
-            <div className='flex-grow'>messages with persons</div>
+            <div className='flex-grow'>{!selectedUserId && (<div className='text-white flex h-full flex-grow items-center justify-center'>
+                <div className=''>&larr; select a person from sidebar</div>
+                </div>)}</div>
             <div className='flex gap-2'>
                 <input type="text" 
                 placeholder='message'
